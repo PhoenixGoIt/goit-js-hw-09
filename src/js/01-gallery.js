6c48cac3fde5d2ae8352585534e5d9f46c168b30
Игнат Лукьянчук <115699352+PhoenixGoIt@users.noreply.github.com>
2023-03-13 13:04:08 +0200

    ch

---
+++ src/js/01-gallery.js
@@ -0,0 +1,21 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryMarkUp = document.querySelector('.gallery');
+
+const galleryEl = galleryItems
+    .map(({ preview, description, original }) =>
+        `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+).join('');
+
+galleryMarkUp.insertAdjacentHTML('beforeend', galleryEl);
+
+const lightbox = new SimpleLightbox('.gallery a',
+ { captionsData:"alt",
+captionDelay: 250,
+captionPosition: "bottom", }
+    
+);
---
+++ src/js/02-video.js
@@ -0,0 +1,23 @@
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+
+player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000));
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime)
+}
+
+
+
+
+//Выдаёт эти ошибки, пытался исправить но ничего не помогло :\
+
+//------------------------------------------------Ошибки------------------------------------------------\\
